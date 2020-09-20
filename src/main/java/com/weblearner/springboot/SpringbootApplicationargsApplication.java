1597f4864e61e571e149bdf18f000c717dccc277
Raghuram04 <46728207+Raghuram04@users.noreply.github.com>
2020-09-19 21:46:52 -0400

    Update SpringbootApplicationargsApplication.java

--- src/main/java/com/weblearner/springboot/SpringbootApplicationargsApplication.java
+++ src/main/java/com/weblearner/springboot/SpringbootApplicationargsApplication.java
@@ -1,28 +1,30 @@
 package com.weblearner.springboot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootApplicationargsApplication implements ApplicationRunner{
+public class SpringbootApplicationargsApplication implements ApplicationRunner {
+	
+	@Value("${name}")
+	private String name;
+	
+	@Value("${location}")
+	private String location;
 
 	public static void main(String[] args) {
-		
-		for(String arg: args) {
-			System.out.println("Getting arg in main class -> "+ arg);
-		}
 		SpringApplication.run(SpringbootApplicationargsApplication.class, args);
 	}
 
 	@Override
-	public void run(ApplicationArguments args) throws Exception {		
+	public void run(ApplicationArguments args) throws Exception {
 		
-		args.getNonOptionArgs().forEach(s -> {
-			System.out.println("Getting arg using ApplicationArgument class --->"+ s);
-		});
-		
+		System.out.println("Name -->"+ name);
+		System.out.println("location -->"+ location);
+
 	}
 
 }
